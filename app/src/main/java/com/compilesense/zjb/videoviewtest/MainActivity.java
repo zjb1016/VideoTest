cc184dd20fed93fd502bd7017613f8d1ab60bab4
zjb1016 <982832751@qq.com>
2018-03-26 13:18:21 +0800

    第二次上传

--- app/src/main/java/com/compilesense/zjb/videoviewtest/MainActivity.java
+++ app/src/main/java/com/compilesense/zjb/videoviewtest/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //132
+    //1321
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
